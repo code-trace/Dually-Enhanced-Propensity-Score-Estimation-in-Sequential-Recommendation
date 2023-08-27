adc7392a7d267f8f0d4dcfd03dc95324b253ac7a
code-trace <46181745+code-trace@users.noreply.github.com>
2023-08-27 17:12:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-import os
+import os   
 
 from setuptools import setup, find_packages
 
